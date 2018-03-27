59df35c09903995f2150367cb4fbd8b0e8c94e1b
Donovan Preston <donovanpreston@gmail.com>
2018-03-27 09:59:10 -0400

    More attempts to fool framebusters

--- child/child.js
+++ child/child.js
@@ -1,6 +1,13 @@
 
 const { classes: Cc, interfaces: Ci, utils: Cu } = Components;
 
+function lognames(obj) {
+  let names = '';
+  for (let n in obj) {
+    names += n + ' ';
+  }
+  console.log(names);
+}
 
 this.tabsplit_child = class API extends ExtensionAPI {
   getAPI(context) {
@@ -9,7 +16,18 @@ this.tabsplit_child = class API extends ExtensionAPI {
         init_child: async () => {
           const doc = context.contentWindow.document;
           const frame = doc.getElementById('browser-iframe');
+          const self = frame.contentWindow.self;
+          Object.defineProperty(frame.contentWindow, 'top', {
+            get: () => { console.log('GET'); return self; }
+          });
           // frame.contentWindow.top = frame.contentWindow.self;
+          console.log(
+            'equal',
+            frame.contentWindow.top === frame.contentWindow.self);
+          //lognames(frame.contentWindow.top);
+          //console.log(
+          //  frame.contentWindow.top.document.body.outerHTML);//,
+          //  frame.contentWindow.self.document.body.outerHTML);
           return "init";
         }
       }
